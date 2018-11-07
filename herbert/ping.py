7ef397e86b7efcb7c8c083ca47a61fc615bc845a
CVTTSD2SI <simonabdellatif@gmx.de>
2018-11-07 22:28:31 +0100

    added ping doc

--- herbert/ping.py
+++ herbert/ping.py
@@ -5,5 +5,8 @@ from basebert import BaseBert
 class PingBert(BaseBert):
     @command(pass_args=False, pass_string=True)
     def ping(self, string):
+        """
+        Pong.
+        """
         self.send_message('pong')
         self.send_message(f'Your string was `"{string}"`')

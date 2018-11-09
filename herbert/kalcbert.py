2836dd2574631040877f987222ef43c8b8235999
AlcedoAttis <drivenbyfusion@gmail.com>
2018-11-09 22:52:49 +0100

    implemented step by step solutions in KalcBert

--- herbert/kalcbert.py
+++ herbert/kalcbert.py
@@ -36,6 +36,34 @@ class KalcBert(ImageBaseBert):
         """
         self.wolfram(string, full=True, steps=False)
 
+    @aliases('stepbystep', 'sbs')
+    @command(pass_string=True)
+    def sbswolfram(self, string):
+        """
+        Show detailed step by step solutions for mathematical input problems
+        """
+        # higly susceptible to fails if WolframAlphas output xml changes
+        # but no scrapy so usable in python 3.7
+        query = quote(string, safe='')
+        url = f'http://api.wolframalpha.com/v2/query?appid=36GXXR-K5UA8L8XTY&input={query}&podstate=Step-by-step%20solution&format=image'
+        _, xdatax = _t_load_content(url) # XML website
+        xdatax = str(xdatax)
+        # check if it worked
+        # find unique point in String relating to the giflink and go on from there to start of link [magic number 44] later
+        # cut string at start of link
+        datax = xdatax[(xdatax.find('Possible intermediate steps')+44):] 
+        # delete xml artifacts and cut string at end of link
+        data, *_ = datax.replace("&amp;", "&").split("'")
+        # check if it worked and a url is found
+        if data[:4] == "http":
+            _, realdata = _t_load_content(data)
+
+            image = Image.open(BytesIO(realdata))
+            self.send_pil_image(image, full=False)
+        else:
+            raise Herberror('no step by step solution feasible')
+
+
     @command(pass_string=True)
     def math(self, string):
         """

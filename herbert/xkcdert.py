a3bb05414751d43ba90eb5a0fd0710a88743b898
nodatapoints <nodatapoints@gmail.com>
2018-11-12 11:13:53 +0100

    dpi dynamicly increased

--- herbert/xkcdert.py
+++ herbert/xkcdert.py
@@ -14,23 +14,25 @@ class XKCDert(BaseBert):
         """
         num = None
         try:
-            num = int(float(string))
+            num = int(float(string))  # ich lasse das einfach mal
         except ValueError:
-            results = hercurles_utils.search_for("xkcd " + string)
+            results = hercurles_utils.search_for('xkcd ' + string)
             for res in results:
-                match = re.match(".*xkcd\.com/(\\d+)", res)
+                match = re.match(r'.*xkcd\.com/(\d+)', res)
                 if match:
                     num = match.group(1)
                     break
 
             if not num:
-                raise Herberror("That is no comic.")
+                raise Herberror('That is no comic.')
 
-        url = f"www.xkcd.com/{num}/info.0.json"
+        url = f'www.xkcd.com/{num}/info.0.json'
         try:
             info_json = json.loads(network.t_load(url).data)
-            self.send_photo(info_json.get('img'), caption=f"{info_json.get('title')}\n\n{info_json.get('alt')}")
+            self.send_photo(
+                info_json.get('img'),
+                caption=f"{info_json.get('title')}\n\n{info_json.get('alt')}"
+            )
+
         except json.decoder.JSONDecodeError:
             raise Herberror("That didn't work out, sorry.")
-
-

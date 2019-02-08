e4e01160f5f3bae654f32ddfa5a64e55793d8e6c
someone else <simonabdllatif@gmx.de>
2019-02-08 18:10:11 +0000

    lowered resolution limit

--- herbert/texbert.py
+++ herbert/texbert.py
@@ -94,7 +94,7 @@ class TexBert(ImageBaseBert):
         string = template.format(string)
 
         target_pixel_width = argvals.get('res') or 1000
-        if target_pixel_width > 1e5:
+        if target_pixel_width > 10000:
             raise Herberror('Dude wtf are you doing?')
 
         try:
